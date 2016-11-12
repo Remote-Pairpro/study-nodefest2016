f87f8d48899d5882b83bf5fd9c28ff41cde0b5e8
kazuhito <kazuhito.sumpic@gmail.com>
2016-11-12 15:49:28 +0900

    リファクタリング。本来の目的である「文字コード未指定fs.readFile()」型で書く。

--- learnyounode/program4.js
+++ learnyounode/program4.js
@@ -3,7 +3,20 @@ const fs = require('fs');
 // 引数処理。
 const path = process.argv[2];
 
-fs.readFile(path, 'utf8', function (err, text) {
+// UTFあり版は、Textが返るから、簡単すぎるやろってことで殺す。
+
+// fs.readFile(path, 'utf8', function (err, text) {
+//     // 改行でぶったぎって、切れた要素数を数える - 1 する。
+//     const count = text.split("\n").length - 1;
+//     // デリミタに改行指定して分割
+//     console.log(count);
+// });
+
+
+// 文字コード未指定版…は、functionの第二引数は、Bufferオブジェクトとなる。
+fs.readFile(path, function (err, buf) {
+    // Bufferオブジェクトは、即刻文字列化。
+    const text = buf.toString();
     // 改行でぶったぎって、切れた要素数を数える - 1 する。
     const count = text.split("\n").length - 1;
     // デリミタに改行指定して分割
