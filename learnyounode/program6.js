caf1d43d4042d521ded4f65d1ba5cf3d163d8325
kazuhito <kazuhito.sumpic@gmail.com>
2016-11-12 16:46:42 +0900

    バグフィックス。仕様の趣旨の理解を間違えてたので、修正。

--- learnyounode/program6.js
+++ learnyounode/program6.js
@@ -2,12 +2,13 @@ const http = require('http');
 
 // 引数処理。
 const url = process.argv[2];
-
-http.get(url, function (response) {
-    response.on('data', function (date) {
+    
+http.get(url, function (res) {
+    res.on('data', function (data) {
+        // XXX わりかし雑いので、あとで治す。
         var html = data.toString();
-        for (var i = 0 ; i < html.length ; i++ ) {
-            console.log(html.charAt(i));
-        }
+        html.split("\n").forEach(line => {
+            console.log(line);
+        });
     });
 });
