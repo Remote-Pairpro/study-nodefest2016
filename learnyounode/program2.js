10062b42d144595bcd84997a28a46253891cd433
kazuhito <kazuhito.sumpic@gmail.com>
2016-11-12 12:30:29 +0900

    リファクタリング。模範解答に寄せるように。

--- learnyounode/program2.js
+++ learnyounode/program2.js
@@ -1,6 +1,6 @@
 var result = 0;
-for (var i = 2 ; i < process.argv.length ; i++) {
-	result = result + parseInt(process.argv[i],10);
+for (var i = 2; i < process.argv.length; i++) {
+	result += Number(process.argv[i], 10);
 }
 console.log(result);
 
